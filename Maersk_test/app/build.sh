bee2bc5efec7540239acbb189db052fd20550e1c
Devika13 <41461243+Devika13@users.noreply.github.com>
2023-05-03 17:59:40 +0200

    Update build.sh

--- Maersk_test/app/build.sh
+++ Maersk_test/app/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ##Preparing build environment
 minikube start --driver=docker
+eval $(minikube docker-env)
+eval $(minikube -p minikube docker-env)
+
+
 
 ##building docker image
 docker build -t flask-app .
